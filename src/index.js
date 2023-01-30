72b23588553abf3c3a0b2ad43268f58c577ede92
SokolKat <SokolskayaKat@yandex.ru>
2023-01-30 16:56:24 +0300

    init: new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const assert = require('assert');
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let mapping = new Map();
+
+  for (let config of bracketsConfig) {
+    mapping.set(config[0], config[1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (mapping.has(char)) {
+      stack.push(char);
+    } else if (stack.length === 0 || char !== mapping.get(stack.pop())) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
